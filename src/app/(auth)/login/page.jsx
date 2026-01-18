8fe6f6957069f5fb8d3431b4f7ecec3d0b9d5358
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-18 22:20:30 +0600

    feat: add Login and LoginForm components for user authentication

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,16 @@
+import LoginForm from "@/components/forms/LoginForm";
+
+const Login = () => {
+    return (
+        <div className='min-h-screen flex justify-center items-center'>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                    <h1 className='text-center text-2xl font-semibold mb-5'>Login</h1>
+                    <LoginForm />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
