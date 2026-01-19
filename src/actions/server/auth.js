cf4fc6acb723ef1061135bcda597ead1a83f0343
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-19 21:16:26 +0600

    feat: add authentication and database connection functionality with MongoDB

---
+++ src/actions/server/auth.js
@@ -0,0 +1,49 @@
+'use server'
+
+import { collections, dbConnect } from "@/lib/dbConnect";
+import bcrypt from 'bcryptjs';
+
+// post user to database | used in register page
+export const postUser = async (payload) => {
+    const { email, password, name } = payload;
+
+    // 1. check payload
+    if (!email || !password) return null;
+
+    // 2. check user
+    const isExists = await dbConnect(collections.USERS).findOne({ email });
+    if (isExists) return null;
+
+    // 3. create user
+    const newUser = {
+        provider: 'credentials',
+        name,
+        email,
+        password: await bcrypt.hash(password, 14),
+        role: 'user',
+        createdAt: new Date().toISOString()
+    };
+
+    // 4. insert user
+    const result = await dbConnect(collections.USERS).insertOne(newUser);
+    if (result.acknowledged) {
+        return {
+            ...result,
+            insertedId: result.insertedId.toString()
+        }
+    }
+} 
+
+export const loginUser = async (payload) => {
+    const { email, password } = payload;
+    if (!email || !password) return null;
+    const user = await dbConnect(collections.USERS).findOne({ email });
+    if (!user) return null;
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+        return user;
+    } else {
+        return null;
+    }
+}
---
+++ src/lib/dbConnect.js
@@ -0,0 +1,27 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+const db_name = process.env.DB_NAME;
+
+// to use collection name without mistake
+export const collections = {
+    PRODUCTS: 'products',
+    USERS: 'users',
+    CART: 'cart',
+    ORDERS: 'orders'
+}
+
+// Create a MongoClient
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+// if no collection: create and connect || only connect collection
+export const dbConnect = (collection_name) => {
+    // if no database then create + connect otherwise connect database
+    return client.db(db_name).collection(collection_name);
+}
