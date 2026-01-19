5ca8aec09e31f38321fe74f2b31b0cc5f7e21c7d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-19 07:31:27 +0600

    feat: restructure layout components and add Navbar, Logo, and Footer components

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,4 +1,5 @@
-import NavLink from './NavLink/NavLink';
+import NavLink from '../NavLink/NavLink';
+import Logo from './Logo';
 
 const Navbar = () => {
 
@@ -21,7 +22,7 @@ const Navbar = () => {
                         {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <Logo />
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
