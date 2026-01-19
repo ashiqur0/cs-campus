95d6319ae834b26502ea582f18ddd6fb241826ea
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-19 21:25:14 +0600

    feat: update Navbar layout for improved responsiveness

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
     return (
         <div className="navbar bg-base-100">
-            <div className="navbar-start">
+            <div className="navbar-start w-full">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
@@ -30,7 +30,7 @@ const Navbar = () => {
                     {links}
                 </ul>
             </div>
-            <div className="navbar-end">
+            <div className="navbar-end md:flex gap-4 hidden w-full">
                 <AuthButtons />
             </div>
         </div>
